67ad204368bcf71253760419485b0daf4da44618
orjon <github@orjon.com>
2020-04-03 09:25:28 +0100

    Initial commit

---
+++ src/Content.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Content extends Component {
+  render(){
+    return(
+      <div className='Content'>Content</div>
+    )
+  }
+}
+
+export default Content;
